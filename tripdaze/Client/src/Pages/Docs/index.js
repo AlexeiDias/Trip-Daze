29c88022110e599c37d5c8dbaf0eba73434c05e5
Alexei Dias <alexeidias@gmail.com>
2019-12-04 00:47:47 -0800

    docs, login, main crated

---
+++ tripdaze/Client/src/Pages/Docs/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+class Docs extends React.Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Docs
